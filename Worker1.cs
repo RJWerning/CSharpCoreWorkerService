e137831fce9573394eb39c04832f3066c8b14a6d
Rich <rjwerning@gmail.com>
2021-07-19 13:42:37 -0500

    start

---
+++ Worker1.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpCoreWorkerService
+{
+    public class Worker1 : BackgroundService
+    {
+        private readonly ILogger<Worker1> _logger;
+
+        public Worker1(ILogger<Worker1> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker1 running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
